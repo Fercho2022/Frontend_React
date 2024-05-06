e9969b50a76ab3afc5e627d52dfc8fe85f4d96e5
fernando <crosio.fernando@gmail.com>
2024-05-06 09:58:31 -0300

    mejora a las funciones componentes con sus tipos

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 
 import './App.css';
-import Card from './Components/Card/Card';
+
+import CardList from './Components/CardList/CardList';
 
 function App() {
   return (
-    <div className="App">
-      <Card />
+    <div>
+      <CardList />
     </div>
   );
 }
--- src/Components/Card/Card.tsx
+++ src/Components/Card/Card.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import "./Card.css";
 
-type Props = {};
+interface Props {
+  companyName: string;
+  ticker: string;
+  price: number;
+}
 
-const Card = (props: Props) => {
+const Card: React.FC<Props> = ({
+  companyName,
+  ticker,
+  price,
+}): JSX.Element => {
   return (
     <div className="card">
       <img
@@ -11,8 +19,10 @@ const Card = (props: Props) => {
         alt=""
       />
       <div className="details">
-        <h2>AAPL</h2>
-        <p>$110</p>
+        <h2>
+          {companyName} ({ticker})
+        </h2>
+        <p>${price}</p>
       </div>
       <p className="info">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit harum
