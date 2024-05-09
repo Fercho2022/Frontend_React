d872ee7cbf2284d8e5f692e3330082acd27449b6
fernando <crosio.fernando@gmail.com>
2024-05-09 08:03:58 -0300

    creacion de rutas y Link para mi pagina web

--- src/App.tsx
+++ src/App.tsx
@@ -1,77 +1,17 @@
-import {
-  BaseSyntheticEvent,
-  ChangeEvent,
-  SyntheticEvent,
-  useState,
-} from "react";
-import "./App.css";
 
-import CardList from "./Components/CardList/CardList";
-import Search from "./Components/Search/Search";
-import { CompanySearch } from "./company";
-import { searchCompanies } from "./api";
-import ListPortfolio from "./Components/Portfolio/ListPortfolio/ListPortfolio";
+import { Outlet } from "react-router";
+import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
-import Hero from "./Components/Hero/Hero";
+
+
 
 function App() {
-  const [search, setSearch] = useState<string>("");
-  const [serverError, setServerError] = useState<string | null>(null);
-  const [searchResult, setSearchResult] = useState<CompanySearch[]>([]);
-  const [portfolioValues, setPortfolioValues] = useState<string[]>([]);
 
-  const HandleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setSearch(e.target.value);
-    console.log(e);
-  };
-
-  const onPortfolioCreate = (e: any) => {
-    e.preventDefault();
-    const exists = portfolioValues.find((value) => value === e.target[0].value);
-    if (exists) return;
-    const updatePortfolio = [...portfolioValues, e.target[0].value];
-    setPortfolioValues(updatePortfolio);
-  };
-
-  const onPortfolioDelete = (e: BaseSyntheticEvent) => {
-    e.preventDefault();
-    const removed = portfolioValues.filter((value) => {
-      return value !== e.target[0].value;
-    });
-
-    setPortfolioValues(removed);
-  };
-
-  const onSearchSubmit = async (e: SyntheticEvent) => {
-    e.preventDefault();
-    const result = await searchCompanies(search);
-    console.log(result);
-    if (typeof result === "string") {
-      setServerError(result);
-    } else if (Array.isArray(result.data)) {
-      setSearchResult(result.data);
-    }
-    console.log(searchResult);
-  };
   return (
-    <div className="App">
-      <Navbar />
-      
-      <Search
-        search={search}
-        onSearchSubmit={onSearchSubmit}
-        HandleSearchChange={HandleSearchChange}
-      />
-      {serverError && <h1>{serverError}</h1>}
-      <ListPortfolio
-        portfolioValues={portfolioValues}
-        onPortfolioDelete={onPortfolioDelete}
-      />
-      <CardList
-        searchResult={searchResult}
-        onPortfolioCreate={onPortfolioCreate}
-      />
-    </div>
+    <>
+    <Navbar/>
+    <Outlet/>
+    </>
   );
 }
 
--- src/Components/Card/Card.tsx
+++ src/Components/Card/Card.tsx
@@ -2,24 +2,31 @@ import React, { SyntheticEvent } from "react";
 import "./Card.css";
 import { CompanySearch } from "../../company";
 import AddPortfolio from "../Portfolio/AddPortfolio/AddPortfolio";
+import { Link } from "react-router-dom";
 
 interface Props {
   id: string;
   searchResult: CompanySearch;
-  onPortfolioCreate: (e: SyntheticEvent)=>void;
- 
+  onPortfolioCreate: (e: SyntheticEvent) => void;
 }
 
-const Card: React.FC<Props> = ({ id, searchResult, onPortfolioCreate}: Props): JSX.Element => {
+const Card: React.FC<Props> = ({
+  id,
+  searchResult,
+  onPortfolioCreate,
+}: Props): JSX.Element => {
   return (
     <div
       className="flex flex-col items-center justify-between w-full p-6 bg-slate-100 rounded-lg md:flex-row"
       key={id}
       id={id}
     >
-      <h2 className="font-bold text-center text-black md:text-left">
+      <Link
+        to={`/company/${searchResult.symbol}`}
+        className="font-bold text-center text-black md:text-left"
+      >
         {searchResult.name} ({searchResult.symbol})
-      </h2>
+      </Link>
       <p className="text-black">{searchResult.currency}</p>
       <p className="font-bold text-black">
         {searchResult.exchangeShortName} - {searchResult.stockExchange}
@@ -29,7 +36,7 @@ const Card: React.FC<Props> = ({ id, searchResult, onPortfolioCreate}: Props): J
         symbol={searchResult.symbol}
       />
     </div>
-);
+  );
 };
 
 export default Card;
